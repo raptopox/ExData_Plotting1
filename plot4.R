b5f425c61b6aa385b39c3b22f4669d033f7f789a
raptopox <raptopoxg@gmail.com>
2014-12-07 14:00:14 +0200

    Commiting changes in plot4.R and plot4.png

--- plot4.R
+++ plot4.R
@@ -43,6 +43,6 @@ lines(data$DateTime, data$Sub_metering_3, type="l", col="blue")
 legend("topright", bty="n", lwd=1, col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
 ##Bottom-right plot
-plot(data$DateTime, data$Global_reactive_power, xlab="", ylab="Global_reactive_power", type="l")
+plot(data$DateTime, data$Global_reactive_power, xlab="datetime", ylab="Global_reactive_power", type="l")
 
 dev.off()
